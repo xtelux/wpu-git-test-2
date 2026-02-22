9936ca5ee3ff426fff074d61ad1312ae75d3c6ff
Margubillah HR <zibril471@gmail.com>
2026-02-22 16:19:03 +0700

    menambah css

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>git remote</title>
+    <link rel="stylesheet" href="sytle.css">
 </head>
 <body>
     <h2>hello world</h2>
